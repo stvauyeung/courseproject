3f8f6a00058e136fc80025c5d3b3b821d0f7a23a
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2013-04-27 11:02:55 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailstutorsProject::Application.config.secret_token = '5c765b20f9ea44e5667b2c16b2fe94b91b5aebdd4fcf1efb6513318f3daae80f81732a0cb7ad5a9d7c65eb3728e2d331b56f6d4c9b61c811cc541e03066c1839'
